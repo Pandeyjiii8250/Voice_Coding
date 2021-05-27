cdf60be4bec2511361a5694a7957a28630403135
Pandeyjiii8250 <rishabhpandey8250@gmail.com>
2021-05-27 11:39:04 +0530

    completed saving file with a butoon

--- src/react/SaveBtn.js
+++ src/react/SaveBtn.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import './SaveBtn.css'
 
 export default function SaveBtn({saveFile}) {
 
     return (
         <div>
-            <h1>Hellow World</h1>
-            <button onClick={saveFile}>Save file</button>
+            {/* <h1>Hellow World</h1> */}
+            {/* <button onClick={saveFile}>Save file</button> */}
+            <div class="btn-container" onClick={saveFile}>
+                <a href="#" class="btn">Save</a>
+            </div>
         </div>
     )
 }
